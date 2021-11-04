21d73d3df9b6861d5e4d61584a587436612ecefa
Simone <smsimone65@gmail.com>
2021-11-04 22:41:13 +0100

    Initial commit

---
+++ src/test/java/it/decimo/prenotation_service/PrenotationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.decimo.prenotation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrenotationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
