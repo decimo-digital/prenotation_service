4b78a7f3c157e17676a0508329028b31cf19fc1f
Simone <64575218+smsimone@users.noreply.github.com>
2021-11-13 19:19:21 +0100

    aggiunto readiness probe

---
+++ src/main/java/it/decimo/prenotation_service/controller/ReadinessController.java
@@ -0,0 +1,16 @@
+package it.decimo.prenotation_service.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ReadinessController {
+    
+    @GetMapping
+    public ResponseEntity<Object> readiness() {
+        return ResponseEntity.ok("Prenotation Service is up and running");
+    }
+}
