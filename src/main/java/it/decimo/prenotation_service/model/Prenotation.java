6f488d5a75250499034a8ef4efcc0ec6b9fc27dc
Simone <64575218+smsimone@users.noreply.github.com>
2021-11-24 19:48:19 +0100

    rimossa column

--- src/main/java/it/decimo/prenotation_service/model/Prenotation.java
+++ src/main/java/it/decimo/prenotation_service/model/Prenotation.java
@@ -36,9 +36,6 @@ public class Prenotation {
     @Column(name = "date")
     private Date dateOfPrenotation;
 
-    @Column(name = "slot")
-    private int slot;
-
     @Column(name = "amount")
     private int amount;
 
