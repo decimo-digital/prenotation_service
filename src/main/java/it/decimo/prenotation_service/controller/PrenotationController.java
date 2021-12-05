f59d62f7bffa0474d0de44cdaf54f5b76cddcd8c
Simone <64575218+smsimone@users.noreply.github.com>
2021-12-05 16:23:45 +0100

    sistemata patch per prenotazione

--- src/main/java/it/decimo/prenotation_service/controller/PrenotationController.java
+++ src/main/java/it/decimo/prenotation_service/controller/PrenotationController.java
@@ -5,7 +5,6 @@ import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -59,7 +58,7 @@ public class PrenotationController {
         return ResponseEntity.ok().body(prenotations);
     }
 
-    @PatchMapping("/")
+    @PostMapping("/update")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "La prenotazione è stata modificata con successo", content = @Content(schema = @Schema(implementation = Prenotation.class))),
             @ApiResponse(responseCode = "404", description = "La prenotazione non esiste", content = @Content(schema = @Schema(implementation = BasicResponse.class))),
